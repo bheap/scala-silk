f8028915a8f7f7c8eb5f4ea58ff8bcf7f5991068
rossputin <ross@bheap.co.uk>
2011-05-31 19:33:45 +0100

    generate views and stream to client

--- synapse-kernel/src/main/scala/application/Application.scala
+++ synapse-kernel/src/main/scala/application/Application.scala
@@ -2,52 +2,40 @@ package com.bheap.synapse.application
 
 import scala.io.Source
 
+import java.io.File
+
 import com.codecommit.antixml._
 
 import unfiltered.request._
 import unfiltered.response._
 
+import com.bheap.synapse.view.XmlView
+
 class Application {
 
-  val pageFiles = (new java.io.File("/Users/rossputin/.synapse/pages")).listFiles.
+  val pageFiles = (new File("/Users/rossputin/.synapse/pages")).listFiles.
     filter(_.isFile).filter(_.getName.endsWith(".xml"))
 
-  val ls: List[(java.lang.String, java.lang.String)] = pageFiles.toList.map {
+  val ls: List[(String, String, String)] = pageFiles.toList.map {
     pageFile =>
       val pageXml = XML fromSource (Source fromURL ("file:%s" format pageFile))
       val url = (pageXml \ 'url \ text).head
-      println("url is : %s" format url)
       println(url.getClass.getName)
       val description = (pageXml \ 'description \ text).head
-      println("description is : %s" format description)
       println(description.getClass.getName)
-      (url, description)
+      val template = (pageXml \ 'template \ text).head
+      println(template.getClass.getName)
+      (url, description, template)
   }
-
-  println("ls is : " + ls)
 
   val filters = ls.map {
     details =>
       unfiltered.filter.Planify {
-        case GET(Path(details._1)) => ResponseString("<html><body><h1>%s</h1></body></html>" format details._2)
+        case GET(Path(details._1)) =>
+          val view = new XmlView(details._3).view
+          ResponseString(view.toString)
       }
   }
-
-  println("filters is : " + filters)
-  println("size of filters is : " + filters.size)
-
-  /*val src = XML fromSource (Source fromURL ("file:/Users/rossputin/.synapse/pages/index.xml"))
-
-  val p1 = unfiltered.filter.Planify {
-    case GET(Path("/")) => Html(<html><body><h1>root path</h1></body></html>)
-  }
-  val p2 = unfiltered.filter.Planify {
-    case GET(Path("/deeper/path")) => Html(<html><body><h1>deeper path</h1></body></html>)
-  }
-  val p3 = unfiltered.filter.Planify {
-    case GET(Path("/deepest/path")) => Html(<html><body><h1>deepest path</h1></body></html>)
-  }
-  val filters = List(p1, p2, p3)*/
 }
 
 object Server {
