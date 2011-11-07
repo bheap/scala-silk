d67b9aaea4d240aef03a17a614875c2e8350865f
Rossputin <ross@bheap.co.uk>
2011-11-07 15:03:14 +0000

    add synapse create

--- synapse-cli/src/main/scala/Synapse.scala
+++ synapse-cli/src/main/scala/Synapse.scala
@@ -1,5 +1,7 @@
 package com.bheap.synapse.interface
 
+import java.io.File
+
 import scopt._
 
 import com.bheap.synapse.application.Application
@@ -10,11 +12,22 @@ object Synapse {
     var config = new Config()
 
     val parser = new OptionParser("synapse") {
-      opt("t", "task", "task is a string property (build|run)", {t: String => config.task = Some(t)})
+      opt("t", "task", "task is a string property (create|build|run)", {t: String => config.task = Some(t)})
     }
 
     if (parser.parse(args)) {
       config.task.get match {
+        case "create" =>
+          val componentDir = new File(System.getProperty("user.dir") + "/component")
+          val resourceDir = new File(System.getProperty("user.dir") + "/resource")
+          val siteDir = new File(System.getProperty("user.dir") + "/site")
+          val templateDir = new File(System.getProperty("user.dir") + "/template")
+          val viewDir = new File(System.getProperty("user.dir") + "/view")
+          if(!(componentDir).exists()) componentDir.mkdir
+          if(!(resourceDir).exists()) resourceDir.mkdir
+          if(!(siteDir).exists()) siteDir.mkdir
+          if(!(templateDir).exists()) templateDir.mkdir
+          if(!(viewDir).exists()) viewDir.mkdir
         case "build" =>
           val gw = new GiftWrap("default.html", "html")
           gw.build
@@ -22,6 +35,8 @@ object Synapse {
           val app = new Application
           app.initialise
           app.start
+        case _ =>
+          println("Sorry, not a valid action, please try (create|build|run)")
       }
     } else {
       println("that was bad okaaaayyyyy ?")
