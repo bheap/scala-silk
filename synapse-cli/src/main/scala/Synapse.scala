9b88b01a9e0161956c196c6703fcab0114ee9e72
Rossputin <ross@bheap.co.uk>
2011-11-08 18:41:26 +0000

    cli feedback and refactor help messages

--- synapse-cli/src/main/scala/Synapse.scala
+++ synapse-cli/src/main/scala/Synapse.scala
@@ -9,10 +9,13 @@ import com.bheap.synapse.tools.{GiftWrap, Preview}
 
 object Synapse {
   def main(args: Array[String]) {
+
+    val tasks = "(create|build|run|preview)"
+
     var config = new Config()
 
     val parser = new OptionParser("synapse") {
-      opt("t", "task", "task is a string property (create|build|run)", {t: String => config.task = Some(t)})
+      opt("t", "task", "task is a string property " + tasks, {t: String => config.task = Some(t)})
     }
 
     if (parser.parse(args)) {
@@ -28,20 +31,21 @@ object Synapse {
           if(!(siteDir).exists()) siteDir.mkdir
           if(!(templateDir).exists()) templateDir.mkdir
           if(!(viewDir).exists()) viewDir.mkdir
+          println("Synapse create complete")
         case "build" =>
           val gw = new GiftWrap("default.html", "html")
           gw.build
+          println("Synapse build complete")
         case "run" =>
           val app = new Application
           app.initialise
           app.start
+          println("Synapse is running")
         case n if n contains "preview-" =>
-          println("initialising preview")
-          println("n is : " + n)
-          println("path is : " + n.split("preview-").last)
           val preview = new Preview(n.split("preview-").last)
+          println("Synapse preview is running")
         case _ =>
-          println("Sorry, not a valid action, please try (create|build|run)")
+          println("Sorry, not a valid action, please try " + tasks)
       }
     } else {
       println("that was bad okaaaayyyyy ?")
