18c0fca7c506f43ce5c20f15f5380a668207d749
Rossputin <ross@bheap.co.uk>
2011-11-18 08:52:22 +0000

    awful first draft of component lookup dilution to comply with css transformation requirements

--- synapse-tools/src/main/scala/GiftWrap.scala
+++ synapse-tools/src/main/scala/GiftWrap.scala
@@ -22,19 +22,21 @@ class GiftWrap(template: String, viewType: String) {
       item =>
         val viewXML = XML.loadFile(item)
         object transformer extends Transformer {
-          val viewDiv = (viewXML \\ "div").filter(item => (item \ "@id").toString.contains("scp-"))
+          val viewDiv = (viewXML \\ "div").filter(item => (item \ "@id").toString.contains("synapse-component"))
           viewDiv.foreach {
 	          comp => 
               val compStruct = (comp \ "@id")(0).toString
-              val cPathBits = compStruct.split("-").last.split("_")
+              val cPathBits = compStruct.split(":").last.split("/")
               val cPath = cPathBits.head
               val cName = cPathBits.last
               val compXML = XML.loadFile(System.getProperty("user.dir") + "/component/" + cPath + "/" + cName + ".html")
               val compDiv = (compXML \\ "div").find(item => (comp \ "@id").text == compStruct) 
-              $("div#" + compStruct).contents = compDiv.get
+              $("div#" + compStruct.replaceAll(":", "").replaceAll("/", "")).contents = compDiv.get
           }
         }
-        val trans = transformer(viewXML)
+        val parsedView = diluteSynapseComponents(item)
+        val parsedXML = XML.loadString(parsedView.get)
+        val trans = transformer(parsedXML)
         (item, trans(0))
     }
   }
@@ -94,5 +96,22 @@ class GiftWrap(template: String, viewType: String) {
       .replace("&quot;", "\"")
       .replace("/&amp;", "&")
       .replace("<html>", "<!doctype html><!--[if lt IE 7 ]><html class=\"no-js ie6\" lang=\"en\"><![endif]--><!--[if IE 7 ]><html class=\"no-js ie7\" lang=\"en\"><![endif]--><!--[if IE 8 ]><html class=\"no-js ie8\" lang=\"en\"><![endif]--><!--[if (gte IE 9)|!(IE)]><!--><html class=\"no-js\" lang=\"en\"><!--<![endif]-->")
+  }
+
+  // load file into string, parse synapse component attribute values ready for css transformation
+  def diluteSynapseComponents(item: File) = {
+    val source = scala.io.Source.fromFile(item)
+    val content = source.mkString
+    source.close
+    val r = """(\"synapse-component:[/a-z-A-Z_0-9:]+\")""".r
+    val matchMap = r.findAllIn(content).toList.map(item => Map(item -> item.replaceAll(":", "").replaceAll("/", "")))
+    // @todo iterate over the items in the map applying each replace based on them within the content string
+    val firstP = if (matchMap.size > 0) Some(content.replace(matchMap.head.keySet.head, matchMap.head.values.head)) else None
+    val secondP = if (matchMap.size > 1) Some(firstP.get.replace(matchMap.last.keySet.head, matchMap.last.values.head)) else None
+    val parsedStr = 
+      if (secondP.isDefined) secondP 
+      else if (firstP.isDefined) firstP
+      else Some(content)
+    parsedStr
   }
 }
