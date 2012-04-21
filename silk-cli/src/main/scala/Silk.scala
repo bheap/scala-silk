130f6fef6d6c2432d0eba871e18573d0b610cc0f
Nick Featch <nick.featch@gmail.com>
2012-04-21 18:22:16 +0100

    soft code clone

--- silk-cli/src/main/scala/Silk.scala
+++ silk-cli/src/main/scala/Silk.scala
@@ -23,7 +23,7 @@ import java.io.File
 import scopt._
 
 import org.silkyweb.pipeline.ViewDrivenPipeline
-import org.silkyweb.utils.{Bundler, Config => SilkConfig}
+import org.silkyweb.utils.{Bundler, Config => SilkConfig, Scout}
 
 /** The CLI.
   *
@@ -72,15 +72,79 @@ object Silk {
     * @param artifactBase File a root directory */
   def artifactClone(prototype: Option[String], artifactName: String, artifactBase: File) {
     if (silkHomeDir.exists) {
-      if (new File(artifactBase + fs + corePkgStr + fs + prototype.getOrElse("nooo")).exists) {
+      val projectId = prototype.getOrElse("nooo")
+      var projects = getProjectsById(artifactBase, artifactName, projectId)
+      if (projects.size > 0) {
+        if (projects.size > 1) {
+          val packages = projects.map(_.pkg).distinct
+          if (packages.size > 1) {
+            val selectedPackage = userSelected(packages, projectId, "package")
+            projects = projects.filter(_.pkg == selectedPackage)
+          }
+          val selectedVersion = autoSelectedVersion(projects.map(_.silkVersion).distinct, projectId)
+          projects = projects.filter(_.silkVersion == selectedVersion)
+        }
         if (artifactName.equals("site-prototype")) {
           if (!localSilkConfigDir.exists) localSilkConfigDir.mkdir
           Bundler.bundleFile(masterSilkConfig, localSilkConfig)
         }
-        Bundler.bundle(new File(artifactBase + fs + corePkgStr + fs + prototype.get + fs + "0.1.0"), userDir)
+        Bundler.bundle(projects.head.baseDir, userDir)
         println("Silk %s : %s clone complete".format(artifactName, prototype.get))
       } else println("Sorry, no artifact found with that id")
     } else println("Please run silk update, your system is not setup properly")
+  }
+
+
+  /** User selects from a list of artifact details (package, version).
+    *
+    * @param choices List[String] list of choices given to user
+    * @param projectId String display the projectId to user
+    * @param selectionType String display the selectionType to user (package, version)
+    * @return String from the list of choices */
+  def userSelected(choices: List[String], projectId: String, selectionType: String): String = {
+    println("The following %ss exist for artifact '%s'".format(selectionType, projectId))
+    for(i <- 0 to choices.size -1) {
+      println("\t%d) %s".format(i+1, choices(i)))
+    }
+    var i = -1
+    while (i < 1 || i > choices.size) {
+      try {
+        println("Please specify the number of the %s you wish to clone: ".format(selectionType))
+        i = readInt 
+        if (i > 0 && i > choices.size) 
+          println("Ooops please pick a number between 1 and  %d.".format(choices.size))
+      } catch {
+        case _ => println("Thats an invalid character, please try again.")
+      }
+    }
+    choices(i - 1)
+  }
+
+  /** Determines silk version. Warns if component or user's silk version are out of date. 
+    *
+    * @param versions List[String] list of versions
+    * @param projectId String display the projectId to user
+    * @return String from the list of choices */
+  def autoSelectedVersion(versions: List[String], projectId: String): String = {
+    var version: String = versions.sortBy(s => s).last
+    val runningSilkVersion = Info.version.split("-")(0) // Strip SNAPSHOT, ALPHA, BETA etc
+    if (version < runningSilkVersion) {
+      userCanAbortClone("WARNING: 'my-site' was constructed with silk version %s ".format(version))
+    } else if (version > runningSilkVersion) {
+      userCanAbortClone("WARNING: This artifact was built with a new version (%s) of silk than yours. Please get the latest version from http://www.silkyweb.org.".format(version))
+      if (versions.size > 1)
+        version = userSelected(versions, projectId, "version")
+    }
+    version
+  }
+
+  /** Warns and allows user abort the clone.
+    *
+    * @param warningMsg String the warning */
+  def userCanAbortClone(warningMsg: String) {
+    println(warningMsg)
+    println("Do you still wish to clone (y/n): ") 
+    if (!readBoolean) sys.exit(0)
   }
 
   /** Install an artifact.
