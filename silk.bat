05eba1ae40567a582e1aa40d72ddc66c901da123
rossputin <ross@bheap.co.uk>
2012-04-26 17:37:45 +0100

    add batch file sample which can handle requisite no. args

---
+++ silk.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set _argNum=0
+
+echo.
+
+for %%i in (%*) do set /A _argNum+=1
+
+if %_argNum% == 2 (
+  start /B java -jar C:\Users\Administrator\bin\silk.jar -t %1 %2
+  goto:_EOF
+)
+
+start /B java -jar C:\Users\Administrator\bin\silk.jar -t %1
+
+:_EOF
+
+echo.
