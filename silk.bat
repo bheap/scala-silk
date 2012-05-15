2bb5d0fd66ede760b82fe1e6ed7a14a4b018c52a
rossputin <ross@bheap.co.uk>
2012-05-15 10:03:03 +0100

    add git clone style to Windows wrapper script

--- silk.bat
+++ silk.bat
@@ -6,6 +6,11 @@ echo.
 
 for %%i in (%*) do set /A _argNum+=1
 
+if %_argNum% == 3 (
+  start /B java -jar C:\Users\Administrator\bin\silk.jar -t %1 %2 %3
+  goto:_EOF
+)
+
 if %_argNum% == 2 (
   start /B java -jar C:\Users\Administrator\bin\silk.jar -t %1 %2
   goto:_EOF
